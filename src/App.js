2676a6e17eab6785432f2f7d714d0cf68a9c52e9
Andita Kaesar <anditakaesar@gmail.com>
2020-01-06 21:47:20 +0700

    update on react-router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,71 @@
 import React from 'react';
+import './App.css';
+import { Menu, Container } from 'semantic-ui-react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import StockComponent from './Stock/StockContainer';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeItem: 'stock'
+    }
+  }
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
+
   render() {
-    return <StockComponent />
+    const { activeItem } = this.state;
+
+    return (
+      <Router>
+        <Menu>
+          <Menu.Item
+            as={Link} to="/"
+            name='stock'
+            active={activeItem === 'stock'}
+            onClick={this.handleItemClick}
+          >
+            Stock
+          </Menu.Item>
+
+          <Menu.Item
+            as={Link} to="/chart"
+            name='chart'
+            active={activeItem === 'chart'}
+            onClick={this.handleItemClick}
+          >
+            Chart
+          </Menu.Item>
+
+          <Menu.Item
+            name='logout'
+            active={activeItem === 'logout'}
+            onClick={this.handleItemClick}
+          >
+            Logout
+          </Menu.Item>
+        </Menu>
+
+        <Switch>
+          <Route path="/chart">
+            <Container className="main-container">
+            <h2>Test Component</h2>
+            </Container>
+          </Route>
+
+          <Route path="/">
+            <StockComponent />
+          </Route>
+        </Switch>
+      </Router>
+      
+    );
   }
 }
 
--- src/Stock/StockContainer.jsx
+++ src/Stock/StockContainer.jsx
@@ -1,47 +1,230 @@
 import React from 'react';
 import 'semantic-ui-css/semantic.min.css';
-import { Menu } from 'semantic-ui-react';
+import { Container, Table, Button, Icon, Input } from 'semantic-ui-react';
+
+const numeral = require('numeral');
+function formatNumber(value, format = '0,0') {
+    return numeral(value).format(format);
+}
 
 class StockComponent extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            activeItem: 'home'
+            stocks: [
+                {
+                    id: 1,
+                    num: 1,
+                    code: 'ANTM',
+                    lot: 10,
+                    currValue: 1000000,
+                    buyPrice: 1000,
+                    buyValue: 1000000,
+                    currPrice: 1000,
+                    pts: 0,
+                    pct: 1.4444
+                },
+                {
+                    id: 2,
+                    num: 2,
+                    code: 'PTBA',
+                    lot: 10,
+                    currValue: 1000000,
+                    buyPrice: 1000,
+                    buyValue: 1000000,
+                    currPrice: 1000,
+                    pts: 0,
+                    pct: 0
+                }
+            ]
         }
     }
 
-    handleItemClick = (e, { name }) => {
-        this.setState({ activeItem: name })
-    };
+    handleStockEdit = (newStock) => {
+        // check the id
+        let editId = this.state.stocks.findIndex(s => s.id === newStock.id);
+        // clone the stocks array
+        let editedStocks = [...this.state.stocks];
+        // set the element with new object
+        editedStocks[editId] = newStock;
+        // set stocks state with new array
+        this.setState({
+            stocks: editedStocks
+        });
+    }
 
     render() {
-        const { activeItem } = this.state;
+        return (
+            <Container className="main-container">
+                <h2>Stock List</h2>
+                <Table celled>
+                    <Table.Header>
+                    <Table.Row>
+                        <Table.HeaderCell>#</Table.HeaderCell>
+                        <Table.HeaderCell>Code</Table.HeaderCell>
+                        <Table.HeaderCell>
+                            Lot<br />
+                            Current Value
+                        </Table.HeaderCell>
+                        <Table.HeaderCell>
+                            Buy Price<br />
+                            Buy Value
+                        </Table.HeaderCell>
+                        <Table.HeaderCell>
+                            Current Price<br />
+                            Pts (+ / -)<br />
+                            %
+                        </Table.HeaderCell>
+                        <Table.HeaderCell></Table.HeaderCell>
+                    </Table.Row>
+                    </Table.Header>
+
+                    <Table.Body>
+                        {this.state.stocks.map(stock => <StockRow key={stock.id} stock={stock} OnSave={this.handleStockEdit} />)}
+                    </Table.Body>
+                </Table>
+            </Container>
+        );
+    }
+}
+
+class StockRow extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            stock: {
+                id: 0,
+                num: 1,
+                code: 'xxxx',
+                lot: 0,
+                currValue: 0,
+                buyPrice: 0,
+                buyValue: 0,
+                currPrice: 0,
+                pts: 0,
+                pct: 0
+            },
+            editMode: false
+        }
+    }
+
+    componentDidMount() {
+        this.setState({
+            stock: this.props.stock
+        });
+    }
+
+    handleEditMode = () => {
+        this.setState({
+            editMode: !this.state.editMode
+        });
+    }
+
+    handleOnChange = (event) => {
+        // clone the stock
+        let editedStock = {...this.state.stock};
+        let value = event.target.value;
+        
+        // set the event.target.name to event.target.value
+        if (event.target.filterType === "number") { // for type number
+            let rgx = new RegExp('^\\d+$');
+            if (rgx.test(value)) {
+                value = parseInt(value);
+                editedStock[event.target.name] = value;
+                this.setState({
+                    stock: editedStock
+                });
+            }
+        } else { // anything else
+            editedStock[event.target.name] = value;
+            this.setState({
+                stock: editedStock
+            });
+        }
+    }
+
+    handleSave = () => {
+        this.props.OnSave(this.state.stock);
+        this.setState({
+            editMode: !this.state.editMode
+        })
+    }
+
+    renderSaveButton() {
+        if (this.state.editMode) {
+            return (
+                <Button onClick={this.handleSave}>
+                    <Icon fitted name='check' />
+                </Button>
+            )
+        } else {
+            return '';
+        }
+    }
+
+    render() {
+        const { stock, editMode } = this.state;
 
         return (
-            <Menu>
-                <Menu.Item header>Our Company</Menu.Item>
-                <Menu.Item
-                name='home'
-                active={activeItem === 'home'}
-                onClick={this.handleItemClick}
-                />
-                <Menu.Item
-                name='aboutUs'
-                active={activeItem === 'aboutUs'}
-                onClick={this.handleItemClick}
-                />
-                <Menu.Item
-                name='jobs'
-                active={activeItem === 'jobs'}
-                onClick={this.handleItemClick}
-                />
-                <Menu.Item
-                name='locations'
-                active={activeItem === 'locations'}
-                onClick={this.handleItemClick}
-                />
-            </Menu>
-        )
+            <Table.Row>
+                <Table.Cell>{stock.num}</Table.Cell>
+                <Table.Cell>{stock.code}</Table.Cell>
+
+                <Table.Cell>
+                    <Table striped>
+                        <Table.Body>
+                            <Table.Row>
+                                <Table.Cell>
+                                    <Input filterType="number" name="lot" disabled={!this.state.editMode} 
+                                    onChange={this.handleOnChange}
+                                    value={stock.lot} />
+                                </Table.Cell>
+                            </Table.Row>
+                            <Table.Row>
+                                <Table.Cell>{formatNumber(stock.currValue)}</Table.Cell>
+                            </Table.Row>
+                        </Table.Body>
+                    </Table>
+                </Table.Cell>
+
+                <Table.Cell>
+                    <Table striped>
+                        <Table.Body>
+                            <Table.Row>
+                                <Table.Cell>{formatNumber(stock.buyPrice)}</Table.Cell>
+                            </Table.Row>
+                            <Table.Row>
+                                <Table.Cell>{formatNumber(stock.buyValue)}</Table.Cell>
+                            </Table.Row>
+                        </Table.Body>
+                    </Table>
+                </Table.Cell>
+
+                <Table.Cell>
+                    <Table striped>
+                        <Table.Body>
+                            <Table.Row>
+                                <Table.Cell>{formatNumber(stock.currPrice)}</Table.Cell>
+                            </Table.Row>
+                            <Table.Row>
+                                <Table.Cell>{formatNumber(stock.pts)}</Table.Cell>
+                            </Table.Row>
+                            <Table.Row>
+                                <Table.Cell>{formatNumber(stock.pct, '0.000')}%</Table.Cell>
+                            </Table.Row>
+                        </Table.Body>
+                        
+                    </Table>
+                </Table.Cell>
+
+                <Table.Cell>
+                    <Button onClick={this.handleEditMode} primary={!editMode} negative={editMode}>
+                        <Icon fitted name='pencil' />
+                    </Button><br /><br />
+                    {this.renderSaveButton()}
+                </Table.Cell>
+            </Table.Row>
+        );
     }
 }
 
